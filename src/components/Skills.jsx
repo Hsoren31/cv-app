a238d19597e2f34e28d0174781fad478d6361aca
Hsoren31 <sorensenhaley@gmail.com>
2025-01-06 14:48:03 -0700

    Feat: create portal/modal for skills form

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,27 +1,63 @@
 import "../styles/Skills.css";
+import { useState } from "react";
+import { createPortal } from "react-dom";
 
-function SkillForm() {
+export default function Skill() {
+  const [showModal, setShowModal] = useState(false);
+  const [skills, setSkills] = useState("");
+  /*Skills should be an array that we will use array.map to render */
   return (
-    <section>
-      <form method="post">
-        <fieldset>
-          <legend>Skills</legend>
+    <>
+      <section id="skills_section">
+        <h2>Skills</h2>
+        <ul>
+          <li>{skills}</li>
+        </ul>
+        <button onClick={() => setShowModal(true)}>Edit</button>
+      </section>
+      {showModal &&
+        createPortal(
+          <section id="skill_form" className="form_portal">
+            <form
+              method="post"
+              onSubmit={(e) => {
+                e.preventDefault();
+                setShowModal(false);
+              }}
+            >
+              <fieldset>
+                <legend>Skills</legend>
 
-          <p>
-            <label htmlFor="skill">Skills</label>
-            <input
-              type="text"
-              name="skill"
-              id="skill"
-              placeholder="Time management"
-            />
-          </p>
-        </fieldset>
-      </form>
+                <p>
+                  <label htmlFor="skill">Skills</label>
+                  <input
+                    value={skills}
+                    onChange={(e) => {
+                      setSkills(e.target.value);
+                    }}
+                    type="text"
+                    name="skill"
+                    id="skill"
+                    placeholder="Time management"
+                  />
+                </p>
 
-      <button type="submit">Save</button>
-    </section>
+                <button onClick={() => setShowModal(false)}>Cancel</button>
+                {/*Cancel button should not save input values. It bypasses the form validation */}
+                <button
+                  type="submit"
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    setShowModal(false);
+                  }}
+                >
+                  Save
+                </button>
+              </fieldset>
+            </form>
+          </section>,
+          document.body
+        )}
+    </>
   );
 }
-
-export { SkillForm };
