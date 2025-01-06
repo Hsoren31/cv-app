f197670f642306243cd070973f9573e4a21205d0
Hsoren31 <sorensenhaley@gmail.com>
2025-01-06 07:14:48 -0700

    Feat: display app components with minor styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,26 @@
 import { useState } from "react";
+import { ContactForm } from "./components/Contact";
+import { EducationForm } from "./components/Education";
+import { ExperienceForm } from "./components/Experience";
+import { SkillForm } from "./components/Skills";
+import { SummaryForm } from "./components/Summary";
 import "./App.css";
+
+/*
+Experience/Education/Skills will need an "add" button
+to add more than one component if necessary
+*/
+
+function App() {
+  return (
+    <section>
+      <ContactForm />
+      <SummaryForm />
+      <ExperienceForm />
+      <SkillForm />
+      <EducationForm />
+    </section>
+  );
+}
+
+export { App };
