22fbcc5e851329cd225c4ebb80cb55746f67e06e
Hsoren31 <sorensenhaley@gmail.com>
2025-01-06 14:24:43 -0700

    Feat: create portal/modal for experience form

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,76 +1,153 @@
 import "../styles/Experience.css";
+import { useState } from "react";
+import { createPortal } from "react-dom";
 
-function ExperienceForm() {
+export default function Experience() {
+  const [showModal, setShowModal] = useState(false);
+  const [company, setCompany] = useState("");
+  const [companyLoc, setCompanyLoc] = useState("");
+  const [companyPos, setCompanyPos] = useState("");
+  const [companyStartTime, setCompanyStartTime] = useState("");
+  const [companyEndTime, setCompanyEndTime] = useState("");
+  const [companyResp, setCompanyResp] = useState("");
+
   return (
-    <section>
-      <form method="post">
-        <fieldset>
-          <legend>Experience</legend>
+    <>
+      <section id="experience_section">
+        <h2>Experience</h2>
+        <div>
+          <h3>{company}</h3>
+          <p>{companyLoc}</p>
+          <p>{companyPos}</p>
+          <p>{companyStartTime + "-" + companyEndTime}</p>
+          {/*
+            Format Dates (Month/Day/Year)
+           + Add check that end time is not before start date
+           + Add a current checkbox that disables end date and returns 'current' */}
+          <p>{companyResp}</p>
+        </div>
+        <button onClick={() => setShowModal(true)}>Edit</button>
+      </section>
+      {showModal &&
+        createPortal(
+          <section id="exp_form" className="form_portal">
+            <form
+              method="post"
+              onSubmit={(e) => {
+                e.preventDefault();
+                setShowModal(false);
+              }}
+            >
+              <fieldset>
+                <legend>Experience</legend>
 
-          <p>
-            <label htmlFor="company">
-              <span aria-label="required">*</span>Company Name
-            </label>
-            <input
-              type="text"
-              name="company"
-              id="company"
-              placeholder="Company A"
-              required
-            />
-          </p>
+                <p>
+                  <label htmlFor="company">
+                    <span aria-label="required">*</span>Company Name
+                  </label>
+                  <input
+                    value={company}
+                    onChange={(e) => {
+                      setCompany(e.target.value);
+                    }}
+                    type="text"
+                    name="company"
+                    id="company"
+                    placeholder="Company A"
+                    required
+                  />
+                </p>
 
-          <p>
-            <label htmlFor="company_location">
-              <span aria-label="required">*</span>Location
-            </label>
-            <input
-              type="text"
-              id="company_location"
-              name="company_location"
-              placeholder="City, State"
-              required
-            />
-          </p>
+                <p>
+                  <label htmlFor="company_location">
+                    <span aria-label="required">*</span>Location
+                  </label>
+                  <input
+                    value={companyLoc}
+                    onChange={(e) => {
+                      setCompanyLoc(e.target.value);
+                    }}
+                    type="text"
+                    id="company_location"
+                    name="company_location"
+                    placeholder="City, State"
+                    required
+                  />
+                </p>
 
-          <p>
-            <label htmlFor="position">
-              <span aria-label="required">*</span>Position
-            </label>
-            <input
-              type="text"
-              name="position"
-              id="position"
-              placeholder="Sales Representative"
-              required
-            />
-          </p>
+                <p>
+                  <label htmlFor="position">
+                    <span aria-label="required">*</span>Position
+                  </label>
+                  <input
+                    value={companyPos}
+                    onChange={(e) => {
+                      setCompanyPos(e.target.value);
+                    }}
+                    type="text"
+                    name="position"
+                    id="position"
+                    placeholder="Sales Representative"
+                    required
+                  />
+                </p>
 
-          <p>
-            <label htmlFor="company_time">
-              <span aria-label="required">*</span>Start & End Date
-            </label>
-            <input
-              type="date"
-              name="company_time"
-              id="company_start"
-              required
-            />
-            <input type="date" name="company_time" id="company_end" required />
-          </p>
+                <p>
+                  <label htmlFor="company_time">
+                    <span aria-label="required">*</span>Start & End Date
+                  </label>
+                  <input
+                    value={companyStartTime}
+                    onChange={(e) => {
+                      setCompanyStartTime(e.target.value);
+                    }}
+                    type="date"
+                    name="company_time"
+                    id="company_start"
+                    required
+                  />
+                  <input
+                    value={companyEndTime}
+                    onChange={(e) => {
+                      setCompanyEndTime(e.target.value);
+                    }}
+                    type="date"
+                    name="company_time"
+                    id="company_end"
+                    required
+                  />
+                </p>
 
-          <p>
-            <label htmlFor="role_description">
-              What were your responsibilities and/or accomplishments?
-            </label>
-            <textarea name="role_description" id="role_description"></textarea>
-          </p>
+                <p>
+                  <label htmlFor="role_description">
+                    What were your responsibilities and/or accomplishments?
+                  </label>
+                  <textarea
+                    value={companyResp}
+                    onChange={(e) => {
+                      setCompanyResp(e.target.value);
+                    }}
+                    name="role_description"
+                    id="role_description"
+                  ></textarea>
+                </p>
 
-          <button type="submit">Save</button>
-        </fieldset>
-      </form>
-    </section>
+                <button onClick={() => setShowModal(false)}>Cancel</button>
+                {/*Cancel button should not save input values. It bypasses the form validation */}
+                <button
+                  type="submit"
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    setShowModal(false);
+                  }}
+                >
+                  Save
+                </button>
+              </fieldset>
+            </form>
+          </section>,
+          document.body
+        )}
+    </>
   );
 }
-
-export { ExperienceForm };
